a7abfaac907b764175da44bfc4aacafe63f7d69d
bhawanakhadka0 <157736698+bhawanakhadka0@users.noreply.github.com>
2024-04-14 14:45:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-const header = document.querySelector("header");
+function printOutput(output){
+    document.getElementById("result").innerText="Result:"+ output;
+}
 
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY> 60)
-});
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-menu.classList.toggle('bx-x');
-navbar.classList.toggle(open);
-};
+function validateInput(num){
+    if (isNaN(num)){
+        alert("Enter a valid number");
+        return false;
+    }
+    return true;
+}
+
+function sum(){
+    var num1 = parseFloat(document.getElementById ("input1").value) ||0;
+    var num2 = parseFloat(document.getElementById ("input2").value) ||0;
+
+    if(!validateInput(num1) || !validateInput(num2)){
+        return;
+    }
+
+    var result=num1+num2;
+    printOutput(result);
+}
+
+function subtract(){
+    var num1 = parseFloat(document.getElementById ("input1").value) ||0;
+    var num2 = parseFloat(document.getElementById ("input2").value) ||0;
+
+    if(!validateInput(num1) || !validateInput(num2)){
+        return;
+    }  
+
+    var result=num1-num2;
+    printOutput(result);
+}
+
+function multiply(){
+    var num1 = parseFloat(document.getElementById ("input1").value) ||0;
+    var num2 = parseFloat(document.getElementById ("input2").value) ||0;
+
+    if(!validateInput(num1) || !validateInput(num2)){
+        return;
+    }  
+    var result=num1*num2;
+    printOutput(result);
+}
+function division(){
+    var num1 = parseFloat(document.getElementById ("input1").value) ||0;
+    var num2 = parseFloat(document.getElementById ("input2").value) ||0;
+
+    if(!validateInput(num1) || !validateInput(num2)){
+        return;
+    }  
+    
+
+if (num2==0){
+    alert("cannot divide by zero");
+    return;
+}
+
+var result= num1/num2;
+printOutput(result);
+}
